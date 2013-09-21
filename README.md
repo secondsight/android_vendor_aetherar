5df0cb8f263df049614dfb7f627e87977841f71f
Builder <builder@android.com>
2013-09-21 04:39:41 -0400

    initial commit for aetherar vendor product

---
+++ README.md
@@ -0,0 +1,3 @@
+- vendor directory for aetherar os - 
+
+Inherit this vendor product to include the aetherar packages and settings
---
+++ config/common.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += vendor/aetherar/prebuilt/CameraWallpaper.apk:system/app/CameraWallpaper.apk
+
+PRODUCT_PACKAGES += 
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aetherar/overlay
