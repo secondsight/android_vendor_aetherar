52dfa659799e821778c470805384e732449157e1
Builder <builder@android.com>
2013-09-23 09:55:55 -0400

    copy CameraWallpaper to system/app and copy its libs to system/lib

--- config/common.mk
+++ config/common.mk
@@ -1,4 +1,12 @@
-PRODUCT_COPY_FILES += vendor/aetherar/prebuilt/CameraWallpaper.apk:data/app/CameraWallpaper.apk
+# prebuilt CameraWallpaper and its native libs
+PRODUCT_COPY_FILES += \
+	vendor/aetherar/prebuilt/apps/CameraWallpaper.apk:system/app/CameraWallpaper.apk \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyi.so:system/lib/libhouyi.so \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyiiconv.so:system/lib/libhouyiiconv.so \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyijpg.so:system/lib/libhouyijpg.so \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyipng.so:system/lib/libhouyipng.so \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyiskia.so:system/lib/libhouyiskia.so \
+	vendor/aetherar/prebuilt/libs/armeabi/libhouyixml.so:system/lib/libhouyixml.so
 
 PRODUCT_PACKAGES += 
 
