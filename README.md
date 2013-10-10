5bc26fafac05b05ff6d2cc6fae09fb54d209382b
Builder <builder@android.com>
2013-10-10 09:57:57 -0400

    * update aetherar to secondsight

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-- vendor directory for aetherar os 
+- vendor directory for secondsight os 
 
 Inherit this vendor product to include the aetherar packages and settings
--- config/common.mk
+++ config/common.mk
@@ -1,13 +1,13 @@
 # prebuilt CameraWallpaper and its native libs
 PRODUCT_COPY_FILES += \
-	vendor/aetherar/prebuilt/apps/CameraWallpaper.apk:system/app/CameraWallpaper.apk \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyi.so:system/lib/libhouyi.so \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyiiconv.so:system/lib/libhouyiiconv.so \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyijpg.so:system/lib/libhouyijpg.so \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyipng.so:system/lib/libhouyipng.so \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyiskia.so:system/lib/libhouyiskia.so \
-	vendor/aetherar/prebuilt/libs/armeabi/libhouyixml.so:system/lib/libhouyixml.so
+	vendor/secondsight/prebuilt/apps/CameraWallpaper.apk:system/app/CameraWallpaper.apk \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyi.so:system/lib/libhouyi.so \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyiiconv.so:system/lib/libhouyiiconv.so \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyijpg.so:system/lib/libhouyijpg.so \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyipng.so:system/lib/libhouyipng.so \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyiskia.so:system/lib/libhouyiskia.so \
+	vendor/secondsight/prebuilt/libs/armeabi/libhouyixml.so:system/lib/libhouyixml.so
 
 PRODUCT_PACKAGES += 
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/aetherar/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/secondsight/overlay
